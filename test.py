42cb0f154f49b642c6e7be101f8b5a7d5b695326
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-07-24 13:28:41 +0930

    #Find Max Number Function
    def max_num(num1, num2, num3):
        arr = [num1, num2, num3]
    
        arr.sort()
        print(arr)
    
        if (num1 > num2 and num1 > num3):
            print(num1)
        elif (num2 > num1 and num2 > num3):
            print(num2)
        else:
            print(num3)
    
    max_num(1,6,3)

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
+#Find Max Number Function 
+
 def max_num(num1, num2, num3):
-    arr = []
-    arr.append(num1,num2, num3)
+    arr = [num1, num2, num3]
+
+
     arr.sort()
     print(arr)
     
-    if (num1 > num2 and num3):
+    if (num1 > num2 and num1 > num3):
         print(num1)
-    elif (num2 > num1 and num3):
-        print(num)
+    elif (num2 > num1 and num2 > num3):
+        print(num2)
+    else:
+        print(num3)
+
+max_num(1,6,3)
