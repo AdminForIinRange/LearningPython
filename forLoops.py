7bfb44c2efd6e407daf4004b4d00513c4ad84659
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-07-24 14:46:30 +0930

    arr = ["mike", "steve", "donald"] print("arr has " + str(len(arr)) + " items indexing from 1")
    for i in range(len(arr)): #redundant because len() exits
        print(i)

--- forLoops.py
+++ forLoops.py
@@ -1,4 +1,5 @@
 arr = ["mike", "steve", "donald"]
+print("arr has " + str(len(arr)) + " items indexing from 1")
 
-for i in range(len(arr)):
+for i in range(len(arr)): #redundant because len() exits 
     print(i)
